eb7fa7bd10267bcbfa8829fe72abc58de966b6ae
a0uda <abdullah.oudaa@gmail.com>
2023-11-28 18:20:53 +0200

    Please accept these changes.

--- CMPN205 Project - Student Version/source/common/ecs/transform.cpp
+++ CMPN205 Project - Student Version/source/common/ecs/transform.cpp
@@ -8,13 +8,17 @@ namespace our {
     // This function computes and returns a matrix that represents this transform
     // Remember that the order of transformations is: Scaling, Rotation then Translation
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
-    glm::mat4 Transform::toMat4() const {
-        //TODO: (Req 3) Write this function
+    glm::mat4 Transform::toMat4() const 
+    {
+
+        //TOD: (Req 3) Write this function
+        //First we created the scale matrix, then the rotation matrix, then translation matrix
+        //We then multiplied them together to get the final matrix which we returned 
         glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), scale);
         glm::mat4 rotationMatrix = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
         glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), position);
         return translationMatrix * rotationMatrix * scaleMatrix;
-        return glm::mat4(1.0f); 
+        
     }
 
      // Deserializes the entity data and components from a json object
