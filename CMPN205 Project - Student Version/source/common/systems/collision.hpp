9f9e32a4dade8d091b11f8089155ec53cf71a4fd
Rawan7544 <110938793+Rawan7544@users.noreply.github.com>
2023-12-29 20:02:22 +0200

    Update collision.hpp
    edited collision

--- CMPN205 Project - Student Version/source/common/systems/collision.hpp
+++ CMPN205 Project - Student Version/source/common/systems/collision.hpp
@@ -1,42 +1,34 @@
 #pragma once
 
-#include "../components/collision.hpp"
+#include "../ecs/component.hpp"
 
 #include <glm/glm.hpp>
-#include <glm/gtc/constants.hpp>
-#include <glm/trigonometric.hpp>
-#include <glm/gtx/fast_trigonometry.hpp>
 
 namespace our
 {
 
-    // The movement system is responsible for moving every entity which contains a MovementComponent.
-    // This system is added as a simple example for how use the ECS framework to implement logic.
-    // For more information, see "common/components/movement.hpp"
-    class CollisionSystem
+    // This component denotes that the MovementSystem will move the owning entity by a certain linear and angular velocity.
+    // This component is added as a simple example for how use the ECS framework to implement logic.
+    // For more information, see "common/systems/movement.hpp"
+    // For a more complex example of how to use the ECS framework, see "free-camera-controller.hpp"
+    class CollisionComponent : public Component
     {
     public:
-        // bool isColliding(glm::vec3 postionA, glm::vec3 postionB, glm::vec3 sizeA, glm::vec3 sizeB)
-        // {
-        //     if (postionA.x + sizeA.x >= postionB.x &&
-        //         postionB.x + sizeB.x >= postionA.x &&
-        //         postionA.y + sizeA.y >= postionB.y &&
-        //         postionB.y + sizeB.y >= postionA.y &&
-        //         postionA.z + sizeA.z >= postionB.z &&
-        //         postionB.z + sizeB.z >= postionA.z)
-        //     {
-        //         return true;
-        //     }
-        //     return false;
-        // }
+        glm::vec3 start, end;
+
+        // The ID of this component type is "Movement"
+        static std::string getID() { return "Collision"; }
+
+        // Reads linearVelocity & angularVelocity from the given json object
+        void deserialize(const nlohmann::json &data) override;
         bool isColliding(glm::vec3 startA, glm::vec3 endA, glm::vec3 startB, glm::vec3 endB)
         {
             if (startA.x <= endB.x &&
                 endA.x >= startB.x &&
                 startA.y <= endB.y &&
                 endA.y >= startB.y &&
-                startA.z <= endB.z &&
-                endA.z >= startB.z)
+                abs(startA.z) <= abs(endB.z) &&
+                abs(endA.z) >= abs(startB.z))
             {
                 return true;
             }
